acd773f4d1ae5dbf06147a824f270721e49c63e9
prasad bandi <bprasad.2009eee@gmail.com>
2025-09-13 23:10:15 +0530

    Delete num.py

--- num.py
+++
@@ -1,7 +0,0 @@
-import time
-from IPython.display import clear_output, display, HTML
-
-for i in range(1, 11):
-    clear_output(wait=True) # Clear the previous output
-    display(HTML(f'<h1 style="font-size: 200px; text-align: center;">{i}</h1>')) # Display the current number with a very large font and center it
-    time.sleep(5) # Wait for 1 second
